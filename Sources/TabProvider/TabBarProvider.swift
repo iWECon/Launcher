9aa28181ebc5d0a6b2c338b215b21077c8dbbd49
iWw <i.1214@yahoo.com>
2020-12-30 10:59:19 +0800

    update

--- Sources/TabProvider/TabBarProvider.swift
+++ Sources/TabProvider/TabBarProvider.swift
@@ -29,7 +29,7 @@ public protocol TabBarProvider {
     
 }
 
-public extension TabBarProvider {
+public extension TabBarProvider where Self: UIViewController {
     
     var tabProviders: [TabProvider] {
         get {
